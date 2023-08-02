f997319c8a024b6fc5545c9818f3fa30ace1d9ff
high-haseeb <haseebkhalidoriginal@gmail.com>
2023-08-02 19:08:11 +0500

    added tailwind

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [query, setQuery] = useState("null");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div className="flex flex-col content-center justify-items-center	">
+      <header className="bg-blue-200">
+        <img src={logo} className="bg-black " alt="logo" />
+        <input type={"text"} onChange={(e)=>setQuery(e.target.value)}></input>
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          className=""
+          href={`https://www.youtube.com/results?search_query=${query}`}
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <h1 className="text-3xl font-bold no-underline my-10">search YOUTUBE</h1>
         </a>
       </header>
     </div>
