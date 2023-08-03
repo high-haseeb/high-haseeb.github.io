67bf794b8b3acc11ee416cd9cedb9d33eaafd6e5
high-haseeb <haseebkhalidoriginal@gmail.com>
2023-08-03 18:24:51 +0500

    testing syncorization

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>This is the home page</div>
+    <div className='text-stone-500 text-8xl font-bold'>This is the home page</div>
   )
 }
 
