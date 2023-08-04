a1d0fa0144d2345f90c79926bad9a42885f5c810
high-haseeb <haseebkhalidoriginal@gmail.com>
2023-08-04 14:29:46 +0500

    testing lazygit

--- app/page.jsx
+++ app/page.jsx
@@ -8,7 +8,7 @@ export default function Home() {
       <div className="h-screen w-full bg-blackC flex items-center justify-center">
         <h1 className="p-5 font-bold text-whiteC text-8xl text-center w-full">
           {" "}
-          Hello next js
+          testing lazy git! does this thing actually work or is is just some farad
         </h1>
       </div>
     </div>
