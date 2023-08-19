fa3b9693a76627ec0561758c58854375b71d195a
high-haseeb <haseebkhalidoriginal@gmail.com>
2023-08-19 22:46:29 +0500

    test api endpoint

--- app/api/user/route.js
+++ app/api/user/route.js
@@ -3,13 +3,13 @@ import { NextResponse } from "next/server"
 
 export async function POST(request) {
   const data = await request.json();
-  const response = await prisma.user.create({
-    data:{
-      name: data.name,
-      email: data.email,
-    }
-  })
-  return NextResponse.json(response)
+  // const response = await prisma.user.create({
+  //   data:{
+  //     name: data.name,
+  //     email: data.email,
+  //   }
+  // })
+  return NextResponse.json(request)
 }
 
 export const dynamic = "force-static";
