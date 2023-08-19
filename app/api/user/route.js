ccf6a75a6e68a4d494a65e95c66c29ed9bc3c272
high-haseeb <haseebkhalidoriginal@gmail.com>
2023-08-19 23:03:34 +0500

    chaned from static to dynamic

--- app/api/user/route.js
+++ app/api/user/route.js
@@ -3,13 +3,13 @@ import { NextResponse } from "next/server"
 
 export async function POST(request) {
   const data = await request.json();
-  // const response = await prisma.user.create({
-  //   data:{
-  //     name: data.name,
-  //     email: data.email,
-  //   }
-  // })
-  return NextResponse.json(request)
+  const response = await prisma.user.create({
+    data:{
+      name: data.name,
+      email: data.email,
+    }
+  })
+  return NextResponse.json(response)
 }
 
-export const dynamic = "force-static";
+// export const dynamic = "force-static";
