40344715817bcc8a3b1366b6194577a647dc420d
high-haseeb <haseebkhalidoriginal@gmail.com>
2024-04-09 03:37:39 +0500

    bug fix

--- app/fluid/page.jsx
+++ app/fluid/page.jsx
@@ -1,25 +1,32 @@
-"use client"
-import React, { useRef, useEffect } from 'react'
-import { Puddle } from "@/components/fluid/puddle"
-import { Canvas } from '@react-three/fiber';
-import Scene from '@/components/AssciExperience';
+"use client";
+import React, { useRef, useEffect } from "react";
+import { Puddle } from "@/components/fluid/puddle";
+import dynamic from "next/dynamic"
+
+const Scene = dynamic(() => import("@/components/AssciExperience"), {
+  ssr: false,
+  loading: () => <p>loading....</p>,
+});
 
 const FluidSim = () => {
   const puddleEl = useRef();
   useEffect(() => {
     if (!puddleEl.current) return;
     let puddle = new Puddle(puddleEl.current);
-    puddle.setNodeSize(24)
+    puddle.setNodeSize(24);
     puddle.setupGrid(); // To initialize the puddle
-    puddle.setNodeStyle("ascii");  // nodeStyle one of ["water", "party", "ascii", "base"]  // Default "base"
-puddle.setDampeningRatio(0.4); // Default 0.8  between 0 and 1
-  },[])
-      return (
-      <div className='bg-[#181818] w-screen h-screen overflow-hidden p-0 m-0'>
-      <Scene/>
-      <div ref={puddleEl} className='w-screen h-screen absolute top-0 left-0 p-0 m-0' />
-      </div>
-      )
-}
+    puddle.setNodeStyle("ascii"); // nodeStyle one of ["water", "party", "ascii", "base"]  // Default "base"
+    puddle.setDampeningRatio(0.4); // Default 0.8  between 0 and 1
+  }, []);
+  return (
+    <div className="bg-[#181818] w-screen h-screen overflow-hidden p-0 m-0">
+      <Scene />
+      <div
+        ref={puddleEl}
+        className="w-screen h-screen absolute top-0 left-0 p-0 m-0"
+      />
+    </div>
+  );
+};
 
-      export default FluidSim
+export default FluidSim;
--- components/fluid/Experience.jsx
+++ components/fluid/Experience.jsx
@@ -1,10 +1,10 @@
+"use client";
 import ASCIIEffect from "@/utils/asciiShader";
 import React, { useRef } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { EffectComposer } from "@react-three/postprocessing";
-import { Center, Environment } from "@react-three/drei";
+import { Center } from "@react-three/drei";
 import { Model } from "./Assci";
-import * as THREE from 'three'
 
 export default function Scene() {
   const asciiEffect = React.useMemo(() => new ASCIIEffect(), []);
@@ -21,24 +21,3 @@ export default function Scene() {
     </div>
   );
 }
-const MouseLight = () => {
-  const ref = useRef();
-  useFrame(({ pointer }) => {
-    ref.current.position.x = pointer.x;
-    ref.current.position.y = pointer.y;
-  });
-  return (
-    <group ref={ref}>
-      <pointLight
-        color={"purple"}
-        position={[0, 0, 1]}
-        intensity={1}
-      />
-      <pointLight
-        color={"purple"}
-        position={[0, 0, -1]}
-        intensity={1}
-      />
-    </group>
-  );
-};
