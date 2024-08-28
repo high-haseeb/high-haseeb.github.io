112d2383cc0d81b8076de96b68ae28461818e938
high-haseeb <haseebkhalidoriginal@gmail.com>
2024-08-28 13:27:43 +0500

    added Scene to not-found

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,15 +1,17 @@
-import Adjectives from "@/components/ui/common/Adjectives";
 import Navbar from "@/components/ui/common/Navbar";
+import Scene from "@/components/three/Scene";
 
 export default function Home() {
   return (
     <main className="bg-brBlack text-brWhite h-screen w-screen p-16  select-none">
       <Navbar />
+
+        <div className="absolute left-0 top-0 z-10 h-screen w-screen pointer-events-none">
+          <Scene />
+        </div>
         <div className="text-[15rem] font-bold leading-[0.85] mt-32">
         <div className="font-meditative text-9xl italic">404-</div>
-          {/* <br /> */}
           <div className="font-black">Not found</div>
-          {/* <br /> */}
           <div className="text-4xl">This page went a little too high and got lost in the clouds. ☁️</div>
         </div>
       </main>
